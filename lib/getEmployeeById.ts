b48f84ddf5b3b0e6aec6227431273456bad3d647
KyleYung <kylehyung@gmail.com>
2025-06-21 16:23:55 -0400

    implemented getPostById.ts

--- lib/getEmployeeById.ts
+++ lib/getEmployeeById.ts
@@ -1,1 +1,32 @@
 // lib/getEmployeeById.ts
+
+import getCollection, {DATA_COLLECTION} from "@/db";
+import {ObjectId} from "mongodb";
+import {EmployeeProps} from "@/types";
+
+export default async function getEmployeeById(id: string) : Promise<EmployeeProps | null> {
+
+    const employeeId = ObjectId.createFromHexString(id);
+
+    const employeeCollection = await getCollection(DATA_COLLECTION);
+
+    const data = await employeeCollection.findOne({ _id: employeeId})
+
+    if (data === null) {
+        return null
+    }
+
+    const employee = {
+        id: id,
+        name: data.name,
+        salary: data.salary,
+        absences: data.absences,
+        otdays: data.otdays,
+        othours: data.othours,
+        weddinghours: data.weddinghours,
+        status: data.status,
+    };
+
+    return employee;
+}
+
