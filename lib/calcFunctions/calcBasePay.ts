a9c705a5c5fdd487a0042c22cd6e600fa518df51
KyleYung <kylehyung@gmail.com>
2025-07-16 17:09:53 -0400

    modified calcTotalPay and calcBasePay to round final number

--- lib/calcFunctions/calcBasePay.ts
+++ lib/calcFunctions/calcBasePay.ts
@@ -3,6 +3,8 @@
 const numDays = 31;
 export default function calcBasePay(salary: number, absences: number): number {
     const dailyRate = salary / numDays;
+    const num = salary - ((absences - 3) * dailyRate);
+    const s = num.toFixed(2);
 
-    return salary - ((absences - 3) * dailyRate);
+    return Number(s);
 }
